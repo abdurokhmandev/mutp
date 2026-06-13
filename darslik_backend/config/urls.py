bd8eb0998db2688f9d101914b73cbc45b9928d70
Abdurahmon <abdurrohmon654@gmail.com>
2026-06-13 20:59:34 +0500

    Fix: Enable Django to serve media and static files in production for Railway deployment

--- darslik_backend/config/urls.py
+++ darslik_backend/config/urls.py
@@ -24,6 +24,11 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+else:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', static_serve, {'document_root': settings.MEDIA_ROOT}),
+        re_path(r'^static/(?P<path>.*)$', static_serve, {'document_root': settings.STATIC_ROOT}),
+    ]
 
     FRONTEND_ROOT = Path(settings.BASE_DIR).parent / 'templates'
 
