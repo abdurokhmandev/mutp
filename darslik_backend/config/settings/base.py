47141cfe82ba0379ec0118f982069169abe71d21
Abdurahmon <abdurrohmon654@gmail.com>
2026-06-15 06:46:47 +0500

    chore: change default bot username settings to mutpbot

--- darslik_backend/config/settings/base.py
+++ darslik_backend/config/settings/base.py
@@ -204,7 +204,7 @@ CACHES = {
 
 # Telegram Bot & OTP settings
 TELEGRAM_BOT_TOKEN = env('TELEGRAM_BOT_TOKEN', default='5839402094:AAF9o_notreal_token_for_dev')
-TELEGRAM_BOT_USERNAME = env('TELEGRAM_BOT_USERNAME', default='EduUzBot')
+TELEGRAM_BOT_USERNAME = env('TELEGRAM_BOT_USERNAME', default='mutpbot')
 OTP_EXPIRE_SECONDS = 300
 OTP_LENGTH = 6
 
